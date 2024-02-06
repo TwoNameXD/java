2398cab58ea3b811fb52e042782e93d13a7fff65
hi <hi@naver.com>
2024-02-06 10:38:47 +0900

    변동

--- src/day02/Ex06.java
+++ src/day02/Ex06.java
@@ -9,11 +9,11 @@ public class Ex06 {
 		int iq = 120;
 		double height = 173.3;
 		
-		System.out.println("이름  : " + str);
-		System.out.println("나이  : " + age);
-		System.out.println("키   : " + height);
-		System.out.println("아이큐 : " + iq);
-		System.out.println("등급  : " + grade);
+		System.out.println("이름 \t: " + str);
+		System.out.println("나이 \t: " + age);
+		System.out.println("키 \t: " + height);
+		System.out.println("아이큐 \t: " + iq);
+		System.out.println("등급 \t: " + grade);
 				
 	}
 }
